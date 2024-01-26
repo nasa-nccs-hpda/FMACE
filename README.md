cfec5b08d663e091655c734151169c4fb28d3a1f
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2024-01-26 11:22:07 -0700

    Create inference script.

--- README.md
+++ README.md
@@ -7,12 +7,25 @@ Foundation Model based on ACE Framework [arxiv:2310.02074](https://arxiv.org/abs
 
 	> conda create --name fme --clone fmod
     > conda activate fme
-    > pip install toolz dask
+    > pip install toolz dask dacite
     > git clone https://github.com/ai2cm/ace.git
     > cd FMACE
     > ln -s ./ace/fme/fme ./fme
 
 	>> cd ../ace/fme
     >> pip install .
+
+## Custom Modulus installation
+
+    >> git clone https://github.com/NVIDIA/modulus.git
+    >> cd modulus
+    >> git checkout b8e27c5c4ebc409e53adaba9832138743ede2785
+    >> git switch -c ace
+    >> make install
+
+## Run Tests
+
+    >> cd FMACE
+    >> pytest
 
 
---
+++ tests/test_registry.py
@@ -0,0 +1,19 @@
+"""The registry also performs configuration set up so it needs to be tested."""
+
+from fme.fcn_training import registry
+
+
+def test_sfno_builder():
+    """Make sure that the monkey patch is going through. Not that this is note
+    testing whether the modulus code runs the code correctly."""
+
+    builder = registry.NET_REGISTRY["SphericalFourierNeuralOperatorNet"]()
+
+    sfno_net = builder.build(1, 1, (16, 32))
+
+    print( f"sfno_net.trans_down.grid = {sfno_net.trans_down.grid}" )
+
+    assert (
+        sfno_net.trans_down.grid == "legendre-gauss"
+        and sfno_net.itrans_up.grid == "legendre-gauss"
+    ), "The grid should be set to legendre-gauss"
